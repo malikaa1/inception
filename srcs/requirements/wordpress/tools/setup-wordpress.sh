cf410cf8028e3ea1d8445b2a85dd836a6f6fd031
mrahmani <mrahmani@student.42.fr>
2023-10-24 23:31:19 +0200

    wordpress

--- srcs/requirements/wordpress/tools/setup-wordpress.sh
+++ srcs/requirements/wordpress/tools/setup-wordpress.sh
@@ -1,22 +1,16 @@
 #!/bin/sh
+cd /var/www/wordpress;
+if ! wp core --allow-root is-installed; then
 
-if [ ! -f "/var/www/wordpress/wp-config.php" ]; then
+    echo "wordpress is not installed , installing ...";
+    wp core --allow-root download --locale=fr_FR --force;
 
-    echo "wordpress doesn't exist, downloading ...";
-    
-    wget https://wordpress.org/wordpress-6.3.2.tar.gz -P /temp/wordpress/
-    tar xfz /temp/wordpress/wordpress-6.3.2.tar.gz -C /var/www/
-    rm -rf /temp/wordpress
-    echo "wordpress downloaded";
-
-    cp /var/www/wordpress/wp-config-sample.php /var/www/wordpress/wp-config.php;
-
-    sed -i "s|define( 'DB_NAME', 'database_name_here' );|define( 'DB_NAME', '$WORDPRESS_DB_NAME' );|" /var/www/wordpress/wp-config.php;
-    sed -i "s|define( 'DB_USER', 'username_here' );|define( 'DB_USER', '$WORDPRESS_DB_USER' );|" /var/www/wordpress/wp-config.php;
-    sed -i "s|define( 'DB_PASSWORD', 'password_here' );|define( 'DB_USER', '$WORDPRESS_DB_USER' );|" /var/www/wordpress/wp-config.php;
-    sed -i "s|define( 'DB_HOST', 'localhost' );|define( 'DB_HOST', '$WORDPRESS_DB_HOST' );|" /var/www/wordpress/wp-config.php;
-
- fi
+    wp config --allow-root create  --force --dbname=$WORDPRESS_DB_NAME --dbuser=$WORDPRESS_DB_USER --dbpass=$WORDPRESS_DB_PASSWORD --dbhost=$WORDPRESS_DB_HOST:$WORDPRESS_DB_PORT;
+    wp core --allow-root install  --locale=fr_FR --url=$WORDPRESS_DOMAIN --title=$WORDPRESS_TITLE --admin_user=$WORDPRESS_ADMIN_USER --admin_password="$WORDPRESS_ADMIN_PASSWORD" --admin_email=$WORDPRESS_ADMIN_EMAIL --skip-email;
+    wp user create --allow-root $WORDPRESS_USER_LOGIN $WORDPRESS_USER_EMAIL --user_pass="$WORDPRESS_USER_PASSWORD" --role=author;
+else
+    echo "Wordpress is already installed";
+fi
 
  echo "Starting php fpm..."; 
  
