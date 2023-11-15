9d745c9020d7d38de68d1e497eb49265a05d1dc2
malikaa <rahmanimalika0@gmail.com>
2023-11-15 23:26:38 +0100

    changements

--- Makefile
+++ Makefile
@@ -1,7 +1,9 @@
 all: 
-	sudo docker-compose -f ./srcs/docker-compose.yml up --build
+	[ -d /home/mrahmani/data/mariadb ] || sudo mkdir -p /home/mrahmani/data/mariadb
+	[ -d /home/mrahmani/data/wordpress ] || sudo mkdir -p /home/mrahmani/data/wordpress
+	docker-compose -f ./srcs/docker-compose.yml up --build
 stop:
-	sudo docker-compose -f ./srcs/docker-compose.yml stop
+	docker-compose -f ./srcs/docker-compose.yml stop
 clean:
 	sudo rm -rf /home/mrahmani/data/*
 re:	stop clean all
--- srcs/requirements/wordpress/tools/setup-wordpress.sh
+++ srcs/requirements/wordpress/tools/setup-wordpress.sh
@@ -3,11 +3,20 @@ cd /var/www/wordpress;
 if ! wp core --allow-root is-installed; then
 
     echo "wordpress is not installed , installing ...";
+    # download wordpress
     wp core --allow-root download --locale=fr_FR --force;
 
+    # create wordpress wp-config.php file 
     wp config --allow-root create  --force --dbname=$WORDPRESS_DB_NAME --dbuser=$WORDPRESS_DB_USER --dbpass=$WORDPRESS_DB_PASSWORD --dbhost=$WORDPRESS_DB_HOST:$WORDPRESS_DB_PORT;
+    
+    # install wordpress (create database) and admin user
     wp core --allow-root install  --locale=fr_FR --url=$WORDPRESS_DOMAIN --title=$WORDPRESS_TITLE --admin_user=$WORDPRESS_ADMIN_USER --admin_password="$WORDPRESS_ADMIN_PASSWORD" --admin_email=$WORDPRESS_ADMIN_EMAIL --skip-email;
+    
+    # create wordpress user.
     wp user create --allow-root $WORDPRESS_USER_LOGIN $WORDPRESS_USER_EMAIL --user_pass="$WORDPRESS_USER_PASSWORD" --role=author;
+
+    # install theme 
+    wp theme --allow-root install royal-elementor-kit --activate
 else
     echo "Wordpress is already installed";
 fi
